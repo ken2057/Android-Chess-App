c2122b02ec3022a28b80c54511ab2bca2948846f
NP712N <npn19998@gmail.com>
2019-08-04 10:29:48 +0700

    Waiting for fix error Error when start activity_sign_up.xml

---
+++ app/src/main/java/com/example/chess/SignUp.java
@@ -0,0 +1,16 @@
+package com.example.chess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.drawable.activity_sign_up);
+    }
+}
