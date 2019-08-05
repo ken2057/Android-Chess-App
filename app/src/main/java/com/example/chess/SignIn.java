b41eed447947b25314e531b11528e0a5b701f97f
NP712N <npn19998@gmail.com>
2019-08-05 16:33:55 +0700

    UI Added Add UI: Sign In, Sign Up, changed gradient theme.

--- app/src/main/java/com/example/chess/SignIn.java
+++ app/src/main/java/com/example/chess/SignIn.java
@@ -5,22 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
-
-public class SignUp extends AppCompatActivity {
-
-    TextView tBtnSignIn;
+public class SignIn extends AppCompatActivity {
+    Button btnSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-        tBtnSignIn=findViewById(R.id.sign_up_to_sign_in);
-        tBtnSignIn.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_sign_in);
+        btnSignIn = findViewById(R.id.sign_in);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent goToSignIn = new Intent(SignUp.this,SignIn.class);
-                startActivity(goToSignIn);
+                Intent goForGame = new Intent(SignIn.this,MainActivity.class);
+                startActivity(goForGame);
                 finish();
             }
         });
